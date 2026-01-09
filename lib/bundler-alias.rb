a1bbbf38b1f1782bf6778ab6cf1adcb692495b34
Ben Ford <binford2k@overlookinfratech.com>
2026-01-09 10:48:29 -0800

    Also alias directly declared gems

--- lib/bundler-alias.rb
+++ lib/bundler-alias.rb
@@ -1,6 +1,5 @@
 require "bundler/alias/version"
 require "bundler/friendly_errors.rb"
-require "yaml"
 
 module Bundler
   module Alias
@@ -13,9 +12,14 @@ module Bundler
 
         @aliases = Hash[Bundler.settings[:aliases].split(',').map {|i| i.split ':' } ]
 
-        Bundler::Plugin.add_hook('before-install-all') do |_d|
+        Bundler::Plugin.add_hook('before-install-all') do |deps|
           require "bundler/alias/dependency_patch"
-          # This hook makes bundler load the plugin and monkey patch as needed.
+          deps.each do |d|
+            next unless Bundler::Alias.aliases.include? d.name
+
+            puts "Gemfile declared #{d.name}, but it has been aliased to #{Bundler::Alias.aliases[d.name]}."
+            d.name = Bundler::Alias.aliases[d.name]
+          end
         end
 
       end
